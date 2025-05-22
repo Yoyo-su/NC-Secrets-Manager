ff4910f850310fb3359d8b125a2d9e961eba46a2
Yoyo-su <iohannan211@gmail.com>
2025-05-22 10:53:20 +0100

    Complete warmup

--- src/warmup.py
+++ src/warmup.py
@@ -9,9 +9,10 @@ def warm_up():
     
     try:
         """Creates an S3 bucket."""
-        # s3client.create_bucket(Bucket=BUCKET_NAME, CreateBucketConfiguration = {"LocationConstraint": 'eu-west-2'})
-        # print(f"Bucket {BUCKET_NAME} Created")
-        
+        s3client.create_bucket(Bucket=BUCKET_NAME, CreateBucketConfiguration = {"LocationConstraint": 'eu-west-2'})
+        print(f"Bucket {BUCKET_NAME} Created")
+        bucket_list = s3client.list_buckets()
+        print(bucket_list["Buckets"])
         """Loads two text files to the bucket."""
         s3client.upload_file("data/text1.txt", BUCKET_NAME, TEXT_1)
         print(f"File uploaded to {BUCKET_NAME}")
@@ -23,6 +24,7 @@ def warm_up():
         contents_list = []
         for file in contents:
             contents_list.append(file['Key'])
+        
         with open(f"{BUCKET_NAME}-contents.txt", "w") as file:
             file.write(f"{BUCKET_NAME} contents: {str(contents_list)}")
             print(f"contents written to file")
@@ -34,9 +36,22 @@ def warm_up():
         """Delete objects from bucket"""
         for item in contents_list:
             s3client.delete_objects(Bucket=BUCKET_NAME, Delete={'Objects':[{'Key': item}]})
+        
         print("objects deleted")
+        
+        """ Deletes the bucket."""
+        s3client.delete_bucket(Bucket=BUCKET_NAME)
+        
+        """Checks that the bucket is deleted by listing the available buckets (there should be none)."""
+        bucket_list = s3client.list_buckets()["Buckets"]
+        if len(bucket_list) == 0:
+            print(f"{BUCKET_NAME} deleted!")
+
+    
     except Exception:
         print("fail")
+        
+       
         
 
 warm_up()
