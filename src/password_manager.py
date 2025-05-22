2674abb64b0402cde9207437a3bcd9f05b675758
Yoyo-su <iohannan211@gmail.com>
2025-05-22 15:58:26 +0100

    implement add secret to application

--- src/password_manager.py
+++ src/password_manager.py
@@ -1,10 +1,12 @@
 from utils.get_user_input import get_user_input
+from utils.add_secret import add_secret
 from utils.exit import check_exit
+import boto3
 
 """Initialise values"""
 user_input = 0
 Check_Exit = False
-
+secrets_client = boto3.client('secretsmanager')
 """Begin application loop - Checks exit command on each iteration"""
 while not Check_Exit:
     Check_Exit = check_exit(user_input)
@@ -16,3 +18,9 @@ while not Check_Exit:
     while user_input == 0:
         user_input = get_user_input()
 
+    if user_input == 'e':
+        user_input = 0
+        secret_name = input("Secret identifier: ")
+        username = input("UserId:   ")
+        password = input("Password: ")
+        add_secret(secrets_client, secret_name, username, password)
