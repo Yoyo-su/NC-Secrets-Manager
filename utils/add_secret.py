75dcb13ee0ca7f1b6e5bd09676b9122a179f62ee
Yoyo-su <iohannan211@gmail.com>
2025-05-22 15:52:30 +0100

    create add_secret util function

---
+++ utils/add_secret.py
@@ -0,0 +1,8 @@
+def add_secret(client, secret_name, username, password):
+    try:
+        secret_string = str({"username": username, "password": password})
+        client.create_secret(Name=secret_name, SecretString=secret_string,)
+        print("Secret saved.")
+    except Exception as error:
+        print(f'Failed to add_secret: {error}')
+        raise Exception
