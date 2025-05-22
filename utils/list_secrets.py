5f653e520056deeceb13c233e70cdb8bb032c581
Yoyo-su <iohannan211@gmail.com>
2025-05-22 16:59:41 +0100

    refactor list_secrets

--- utils/list_secrets.py
+++ utils/list_secrets.py
@@ -1,7 +1,11 @@
 def list_secrets(client):
-    secret_list = client.list_secrets()['SecretList']
-    secret_count = len(secret_list)
-    print(f'{secret_count} secret(s) available')
-    for secret in secret_list:
-        print(secret['Name'])
-    return secret_list
+    try:
+        secret_list = client.list_secrets()['SecretList']
+        secret_count = len(secret_list)
+        print(f'{secret_count} secret(s) available')
+        for secret in secret_list:
+            print(secret['Name'])
+        return secret_list
+    except Exception as error:
+        print(f'Failed to list secrets: {error}')
+        raise error
