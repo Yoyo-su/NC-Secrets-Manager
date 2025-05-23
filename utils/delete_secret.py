db1f569a5ada304d40817dc09100c1a73a910f08
fmunyaneza <fmunyaneza.fm@gmail.com>
2025-05-23 10:55:13 +0100

    creat delete function

---
+++ utils/delete_secret.py
@@ -0,0 +1,10 @@
+
+def delete_secret(client,secret_id):
+    try:
+        client.delete_secret(
+        SecretId=secret_id,
+        ForceDeleteWithoutRecovery=True)
+        print("Secret deleted.")
+    except Exception as error:
+        print(f'Failed to delete secret: {error}')
+        raise Exception
